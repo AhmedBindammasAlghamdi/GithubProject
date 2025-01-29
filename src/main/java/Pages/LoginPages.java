9f863d1dc0a2cc5a2646faf0d57e4acd1ceae613
Ahmed-alghamdi <ahmad.bindammas@gmail.com>
2025-01-29 21:09:09 +0300

    added new future

--- src/main/java/Pages/LoginPages.java
+++ src/main/java/Pages/LoginPages.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import static OjectsPath.GithubPaths.*;
+import static OjectsPath.LoginPaths.*;
 
 public class LoginPages {
         WebDriver driver = null ;
@@ -16,12 +17,26 @@ public class LoginPages {
     }
 
     public void clickOnSignInBtn(){
-        WebElement SignInBtn = driver.findElement(signIn);
+        WebElement SignInBtn = driver.findElement(signInBtn);
         SignInBtn.click();
     }
     public void clickOnSignUpBtn(){
         WebElement SignUpBtn = driver.findElement(signUp);
         SignUpBtn.click();
+    }
+    public void fillEmail(){
+        WebElement Email = driver.findElement(username);
+        Email.sendKeys("ahmad.bindammas@gmail.com");
+    }
+
+    public void fillPassword(){
+        WebElement Email = driver.findElement(password);
+        Email.sendKeys("Amdg0898");
+    }
+
+    public void ClickOnLogin(){
+        WebElement login = driver.findElement(signIn);
+        login.click();
     }
 }
 
